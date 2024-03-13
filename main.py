4fe978467d8669d444b570b218a0b4dee7aecda7
bonzlynoel <156226279+bonzlynoel@users.noreply.github.com>
2024-03-13 11:32:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ if __name__ == '__main__':
     print_hi('test github ')
     print(f'test réussi')
     print('123')
-    456 789
+    456 789 10
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
