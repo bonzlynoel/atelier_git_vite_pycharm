6c48965e0a90f31dc9f67777f11a0baa70358623
bonzlynoel <bonzly@outlook.com>
2024-03-13 11:31:59 -0400

    test

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ if __name__ == '__main__':
     print_hi('test github ')
     print(f'test réussi')
     print('123')
-    456
+    456 789
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
